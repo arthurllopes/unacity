5dbf387a5b87a6bd5778e680095d5256435ee909
Arthur Lopes <arthurllopes10@gmail.com>
2022-02-22 21:25:51 -0300

    'setup'

---
+++ styles/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export const theme = extendTheme({
+    styles: {
+        global: {
+            body: {
+                bg: 'gray.50',
+            }
+        }
+    },
+    fonts: {
+        heading: 'Roboto',
+        body: 'Roboto'
+    }
+})
