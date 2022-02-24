58e281067d1a7f8f8c912eda0667236a499249e8
Arthur Lopes <arthurllopes10@gmail.com>
2022-02-24 19:02:24 -0300

    added font + global style

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,15 +1,36 @@
 import { extendTheme } from "@chakra-ui/react"
 
 export const theme = extendTheme({
+    colors: {
+        brand: {
+            purple: "#B588EB",
+            bebe: "#97F5E2",
+            yellow: "#EDD258",
+            pink: "#F098CF",
+            blue: "#8F98EB",
+        },
+    },
     styles: {
         global: {
             body: {
                 bg: 'gray.50',
             }
         }
+
     },
     fonts: {
-        heading: 'Roboto',
-        body: 'Roboto'
-    }
+        heading: 'DM Sans',
+        body: 'DM Sans'
+    },
+    layerStyles: {
+        title: {
+          fontWeight: '700',
+        },
+        text: {
+            fontWeight: '500',
+        },
+        description: {
+            fontWeight: '400',
+        },
+    },
 })
