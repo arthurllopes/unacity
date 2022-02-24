8aff9e91f9762df9134412b762e6ac7741a16eaa
Arthur Lopes <arthurllopes10@gmail.com>
2022-02-24 20:03:48 -0300

    home base

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,8 +1,27 @@
+import { Box, Center, Container, Divider, Flex, Spacer } from '@chakra-ui/react'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Container as="header" p={6} maxW="auto" display="flex" alignItems="center" bg="gray.100" >
+        <div className="logo">
+          LOGO
+        </div>
+          <Spacer />
+        <Flex as="nav" d="flex" alignItems="center" justifyContent="spaceBetween">
+          <Box px='2'>
+            Home
+          </Box>
+          <Box>
+          <Center height='20px' bg="gray.400">
+            <Divider orientation='vertical'  />
+          </Center>
+          </Box>
+          <Box px='2'>
+            Serviços
+          </Box>
+        </Flex>
+    </Container>
   )
 }
 
